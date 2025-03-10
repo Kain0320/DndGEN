8ea071b0329c27aeeeb88b7d65441c820a73043f
Kain0320 <vip.turkanov21@gmail.com>
2025-03-10 16:06:40 +0000

    12

---
+++ spells.py
@@ -0,0 +1,39 @@
+
+class_spells = {
+    "Wizard": [
+        "Mage Hand", "Magic Missile", "Shield", "Fire Bolt", "Burning Hands",
+        "Detect Magic", "Sleep", "Mage Armor", "Chromatic Orb", "Shield"
+    ],
+    "Cleric": [
+        "Cure Wounds", "Bless", "Guiding Bolt", "Healing Word", "Detect Magic",
+        "Spiritual Weapon", "Sacred Flame", "Protection from Evil and Good"
+    ],
+    "Sorcerer": [
+        "Fire Bolt", "Mage Hand", "Shield", "Mage Armor", "Burning Hands",
+        "Magic Missile", "Chromatic Orb", "Counterspell", "Detect Magic"
+    ],
+    "Bard": [
+        "Vicious Mockery", "Healing Word", "Charm Person", "Disguise Self", "Dissonant Whispers",
+        "Tasha's Hideous Laughter", "Cure Wounds", "Faerie Fire", "Heroism"
+    ],
+    "Warlock": [
+        "Eldritch Blast", "Mage Hand", "Hex", "Armor of Agathys", "Shield",
+        "Hellish Rebuke", "Detect Magic", "Burning Hands", "Darkness"
+    ],
+    "Druid": [
+        "Druidcraft", "Produce Flame", "Healing Word", "Entangle", "Thorn Whip", 
+        "Faerie Fire", "Goodberry", "Cure Wounds", "Moonbeam", "Barkskin"
+    ],
+    "Paladin": [
+        "Bless", "Shield", "Cure Wounds", "Divine Smite", "Wrathful Smite",
+        "Protection from Evil and Good", "Lay on Hands", "Thunderous Smite"
+    ],
+    "Ranger": [
+        "Hunter's Mark", "Cure Wounds", "Entangle", "Detect Magic", "Faerie Fire", 
+        "Hail of Thorns", "Goodberry", "Absorb Elements", "Spike Growth"  
+    ],
+     "Fighter": ["None"],  # Fighter doesn't have spells
+    "Barbarian": ["None"],  # Barbarian doesn't have spells
+    "Monk": ["None"],  # Monk doesn't have spells
+    "Rogue": ["None"],  # Rogue doesn't have spells
+}
